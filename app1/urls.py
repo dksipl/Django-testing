0885938bf5d45046b17743e8e223e109009ceeda
dksipl <dhruv.samyak45@gmail.com>
2022-09-13 16:51:48 +0530

    .

--- app1/urls.py
+++ app1/urls.py
@@ -3,7 +3,6 @@ from django.urls import path
 from app1 import views as app1_views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
     path('', app1_views.home, name = 'app1-home'),
     path('display/', app1_views.display, name = 'app1-display'),
 ]
